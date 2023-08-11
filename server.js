869a47d0bd7254faf97ca54e93bf9fd1c9fb6f98
YigitK00 <yigitklc123@gmail.com>
2023-08-10 18:35:34 -0700

    initial commit

---
+++ server.js
@@ -0,0 +1,31 @@
+const express = require("express");
+const cors = require("cors");
+const mongoose = require("mongoose");
+//require('dotenv').config();
+
+const app = express();
+const port = 5000;
+
+app.use(cors());
+app.use(express.json());
+
+const todoRouter = require("./routes/todos");
+app.use("/todos", todoRouter);
+
+//const url = process.env.ATLAS_URI;
+const url =
+  "mongodb+srv://yigitklc123:12345@cluster1.h2xbl8k.mongodb.net/?retryWrites=true&w=majority";
+
+mongoose.connect(url, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+const connection = mongoose.connection;
+connection.once("open", () => {
+  console.log("MongoDB database connection established successfully");
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
